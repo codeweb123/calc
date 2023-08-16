b30b07ed01dabaad7a1887fab504f5e1be2e9cca
codeweb123 <jiyonj@hotmail.com>
2023-08-16 10:14:49 -0500

    addDecimal function

--- script.js
+++ script.js
@@ -2,17 +2,51 @@ const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
 
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-  console.log(number);
+  //if current display value is 0, replace it, if not add number
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    displayValue === "0" ? number : displayValue + number;
+}
+
+function addDecimal() {
+  //if no decimal add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  if (!firstValue) {
+    firstValue = currentValue;
+  }
+  operatorValue = operator;
+  console.log("firstValue", firstValue);
+  console.log("operator", operatorValue);
 }
 
 // Add event listeners for numbers, operators, decimal buttons
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
-    inputBtn.addEventListener("click", sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("operator")) {
-    inputBtn.addEventListener("click", sendNumberValue(inputBtn.value));
-  } else if (inputBtn.classList.contains("operator")) {
-    inputBtn.addEventListener("click", sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", () => useOperator(inputBtn.value));
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", () => addDecimal());
   }
 });
+
+//reset all values, display
+function resetAll() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
+  calculatorDisplay.textContent = "0";
+}
+
+clearBtn.addEventListener("click", resetAll);
